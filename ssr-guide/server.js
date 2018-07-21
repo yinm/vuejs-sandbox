b5c4115926198ff38db9716ce17cc40bdcae4df1
yinm <yinm.cminor9@gmail.com>
2018-07-21 11:48:10 +0900

    Check tempalte

--- ssr-guide/server.js
+++ ssr-guide/server.js
@@ -1,6 +1,6 @@
 const Vue = require('vue')
 const server = require('express')()
-const renderer = require('vue-server-renderer').createRenderer()
+const createRenderer = require('vue-server-renderer').createRenderer
 
 server.get('*', (req, res) => {
   const app = new Vue({
@@ -10,19 +10,16 @@ server.get('*', (req, res) => {
     template: `<div>The visited URL is: {{ url }}</div>`,
   })
 
+  const renderer = createRenderer({
+    template: require('fs').readFileSync('./index.template.html', 'utf-8')
+  })
+
   renderer.renderToString(app, (err, html) => {
     if (err) {
       res.status(500).end('Internal Server Error')
       return
     }
-
-    res.end(`
-      <!doctype html>
-      <html lang="en">
-        <head><title>Hello</title></head>
-        <body>${html}</body>
-      </html>
-    `)
+    console.log(html)
   })
 })
 
