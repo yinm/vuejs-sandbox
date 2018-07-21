cfbbffdd68ddbcf40e0c7ea01fa3543bbe262084
yinm <yinm.cminor9@gmail.com>
2018-07-21 11:27:44 +0900

    Render with express

--- ssr-guide/server.js
+++ ssr-guide/server.js
@@ -1,12 +1,32 @@
 const Vue = require('vue')
-const app = new Vue({
-  template: '<div>Hello World</div>'
-})
-
+const server = require('express')()
 const renderer = require('vue-server-renderer').createRenderer()
 
-renderer.renderToString(app).then(html => {
-  console.log(html)
-}).catch(err => {
-  console.error(err)
+server.get('*', (req, res) => {
+  const app = new Vue({
+    data: {
+      url: req.url,
+    },
+    template: `<div>The visited URL is: {{ url }}</div>`,
+  })
+
+  renderer.renderToString(app, (err, html) => {
+    if (err) {
+      res.status(500).end('Internal Server Error')
+      return
+    }
+
+    res.end(`
+      <!doctype html>
+      <html lang="en">
+        <head><title>Hello</title></head>
+        <body>${html}</body>
+      </html>
+    `)
+  })
 })
+
+const port = 8080
+
+server.listen(port)
+console.log(`server is listening on ${port} port`)
