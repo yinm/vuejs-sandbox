29ce42ff4cbfa93bf3e8b05cda4fe21f20924a13
yinm <yinm.cminor9@gmail.com>
2018-07-23 08:37:23 +0900

    Add api client of client and server

--- ssr-sample/build/webpack.client.config.js
+++ ssr-sample/build/webpack.client.config.js
@@ -3,6 +3,11 @@ const base = require('./webpack.base.config')
 const HTMLPlugin = require('html-webpack-plugin')
 
 const config = Object.assign({}, base, {
+  resolve: {
+    alias: Object.assign({}, base.resolve.alias, {
+      'create-api': './create-api-client.js'
+    })
+  },
   plugins: (base.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env.VUE_ENV': '"client"'
