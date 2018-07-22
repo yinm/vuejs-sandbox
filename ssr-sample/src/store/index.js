03d3b7b7613d01608430d8c40c6a6b5c3546256e
yinm <yinm.cminor9@gmail.com>
2018-07-23 08:29:40 +0900

    Update for weather API

--- ssr-sample/src/store/index.js
+++ ssr-sample/src/store/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { fetchWeather } from './api'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
+    weather: {
+      title: '',
+      state: ''
+    }
   },
+
   actions: {
+    SHOW_WEATHER: ({ state, dispatch }) => {
+      if (state.weather.title.length !== 0) {
+        return Promise.resolve()
+      }
+      return dispatch('FETCH_WEATHER')
+    },
+    FETCH_WEATHER: ({ commit }) => {
+      return fetchWeather()
+        .then((weather) => {
+          commit('SET_WEATHER', weather)
+        })
+    }
   },
+
   mutations: {
+    SET_WEATHER: (state, weather) => {
+      state.weather = weather
+    }
   },
+
   getters: {
   }
 })
