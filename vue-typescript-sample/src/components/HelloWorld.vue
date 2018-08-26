8e74492d131da4f79350012714e29de02a98158a
yinm <yinm.cminor9@gmail.com>
2018-08-26 11:59:32 +0900

    Add TS ver

--- vue-typescript-sample/src/components/HelloWorld.vue
+++ vue-typescript-sample/src/components/HelloWorld.vue
@@ -6,59 +6,56 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: 'hello-vue',
+<script lang="ts">
+import { Component, Prop, Emit, Watch, Vue } from 'vue-property-decorator'
 
-  props: ['val'],
-
-  data() {
-    return {
-      value: this.val,
-      inputValue: '',
-    }
-  },
-
-  mounted() {
-    console.log('mounted')
-  },
-
-  computed: {
-    isDisabled() {
-      return this.inputValue === ''
-    },
-  },
-
-  methods: {
-    handleInput($event) {
-      this.inputValue = $event.target.value
-    },
-
-    handleClick() {
-      if (this.inputValue === '') {
-        return
-      }
-      this.value = this.inputValue
-      this.inputValue = ''
-
-      this.$emit('handle-click', this.value)
-    },
-  },
-
-  watch: {
-    value(newValue, oldValue) {
-      console.log(`watch: ${newValue}, ${oldValue}`)
-    },
-  },
-
+@Component({
   filters: {
-    convertUpperCase(value) {
-      if (!value) return
+    convertUpperCase(value: string): string | null {
+      if (!value) return null
       return value.toUpperCase()
     },
   },
+})
+
+export default class HelloVue extends Vue {
+  @Prop() private val!: string
+
+  @Emit('handle-click')
+  clickButton(val: string): void {}
+
+  @Watch('value')
+  onValueChange(newValue: string, oldValue: string): void {
+    console.log(`watch: ${newValue}, ${oldValue}`)
+  }
+
+  value: string = this.val
+  inputValue: string = ''
+
+  // lifecycle hook
+  mounted(): void {
+    console.log('mounted')
+  }
+
+  // computed
+  get isDisabled(): boolean {
+    return this.inputValue === ''
+  }
+
+  // methods
+  handleInput($event: any): void {
+    this.inputValue = $event.target.value
+  }
+  handleClick(): void {
+    if (this.inputValue === '') {
+      return
+    }
+    this.value = this.inputValue
+    this.inputValue = ''
+    this.clickButton(this.value)
+  }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 </style>
