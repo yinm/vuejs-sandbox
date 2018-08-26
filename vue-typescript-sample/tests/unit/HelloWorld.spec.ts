a91209496245f31d10d4950bd424f2d7f52666c2
yinm <yinm.cminor9@gmail.com>
2018-08-26 12:06:52 +0900

    Add test

--- vue-typescript-sample/tests/unit/HelloWorld.spec.ts
+++ vue-typescript-sample/tests/unit/HelloWorld.spec.ts
@@ -1,12 +1,16 @@
-import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import { Wrapper, shallowMount } from '@vue/test-utils'
+import HelloVue from '@/components/HelloVue.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+describe('HelloVue.vue', () => {
+  let wrapper: Wrapper<HelloVue>
+
+  it('propsを受け取る値のテスト', () => {
+    const val = 'Vue'
+    wrapper = shallowMount(HelloVue, {
+      propsData: { val },
+    })
+
+    expect(wrapper.props().val).toBe(val)
+    expect(wrapper.text()).toMatch('Hello VUE')
+  })
+})
