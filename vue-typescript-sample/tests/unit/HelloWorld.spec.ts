abadb594ceee4ac58aa0f64966398e5d8b7672df
yinm <yinm.cminor9@gmail.com>
2018-08-26 12:38:14 +0900

    Add tests

--- vue-typescript-sample/tests/unit/HelloWorld.spec.ts
+++ vue-typescript-sample/tests/unit/HelloWorld.spec.ts
@@ -13,4 +13,22 @@ describe('HelloVue.vue', () => {
     expect(wrapper.props().val).toBe(val)
     expect(wrapper.text()).toMatch('Hello VUE')
   })
+
+  it('描画されるDOMのテスト', () => {
+    wrapper = shallowMount(HelloVue)
+
+    expect(wrapper.contains('h1')).toBeTruthy()
+    expect(wrapper.contains('input')).toBeTruthy()
+    expect(wrapper.contains('button')).toBeTruthy()
+  })
+
+  it('ボタンの非活性のテスト', () => {
+    wrapper = shallowMount(HelloVue)
+    const button = wrapper.find('button')
+
+    wrapper.setData({ inputValue: '' })
+
+    expect(button.element.getAttribute('disabled')).toBeTruthy()
+  })
+
 })
