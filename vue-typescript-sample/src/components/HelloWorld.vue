be3f87fe55b5ae7e04dd4dd6b27afa6ef4d4227e
yinm <yinm.cminor9@gmail.com>
2018-08-26 11:45:19 +0900

    Add JS ver

--- vue-typescript-sample/src/components/HelloWorld.vue
+++ vue-typescript-sample/src/components/HelloWorld.vue
@@ -1,59 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1>Hello {{ value | convertUpperCase }}</h1>
+    <input type="text" :value="inputValue" @input="handleInput($event)" />
+    <button @click="handleClick()" :disabled="isDisabled">button</button>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+<script>
+export default {
+  name: 'hello-vue',
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+  props: ['val'],
+
+  data() {
+    return {
+      value: this.val,
+      inputValue: '',
+    }
+  },
+
+  mounted() {
+    console.log('mounted')
+  },
+
+  computed: {
+    isDisabled() {
+      return this.inputValue === ''
+    },
+  },
+
+  methods: {
+    handleInput($event) {
+      this.inputValue = $event.target.value
+    },
+
+    handleClick() {
+      if (this.inputValue === '') {
+        return
+      }
+      this.value = this.inputValue
+      this.inputValue = ''
+
+      this.$emit('handle-click', this.value)
+    },
+  },
+
+  watch: {
+    value(newValue, oldValue) {
+      console.log(`watch: ${newValue}, ${oldValue}`)
+    },
+  },
+
+  filters: {
+    convertUpperCase(value) {
+      if (!value) return
+      return value.toUpperCase()
+    },
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped>
 </style>
